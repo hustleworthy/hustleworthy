71349078084275b93b8f73d6d0f828cd45c8d833
hustleworthy <info@hustleworthy.com>
2025-09-02 16:34:07 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import './custom.css'
 import Header from '@/components/Header'
 import Providers from '@/components/Providers'
+import Google from 'next-auth/providers/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,16 +34,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="256x256" href="/android-chrome-256x256.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#03a9f4" />
         <meta name="msapplication-TileColor" content="#03a9f4" />
         <meta name="msapplication-TileImage" content="/android-chrome-192x192.png" />
+        {/* Google tag (gtag.js) */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2JRGYFELTP"></script>
+<script dangerouslySetInnerHTML={{
+  __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-2JRGYFELTP');
+  `
+}} />
       </head>
       <body className={inter.className}>
         <Providers>
