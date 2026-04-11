4334c4ab74917e27af367617893c49de0c2047a0
hustleworthy <info@hustleworthy.com>
2026-04-11 23:34:31 +0530

    fix responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
